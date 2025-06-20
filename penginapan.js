b34e76ce4600292ddff1228082d082ab53d24e5a
Luaor0809 <sabaruddinjaya07@gmail.com>
2025-06-20 12:58:57 +0800

    Create penginapan.js

---
+++ penginapan.js
@@ -0,0 +1,45 @@
+{
+  "type": "Compass Connections",
+  "version": {
+    "$numberInt": "1"
+  },
+  "connections": [
+    {
+      "id": "62033d71-2ae8-4ff4-8b0b-371509f17356",
+      "lastUsed": {
+        "$date": {
+          "$numberLong": "1750337975197"
+        }
+      },
+      "favorite": {
+        "name": "NurRamadhani"
+      },
+      "savedConnectionType": "recent",
+      "connectionOptions": {
+        "connectionString": "mongodb://localhost:27017/",
+        "oidc": {
+          "serializedState": "eyJvaWRjUGx1Z2luU3RhdGVWZXJzaW9uIjowLCJzdGF0ZSI6W119"
+        }
+      }
+    },
+    {
+      "id": "a5121e81-fe6f-475b-895e-059eecdfaa79",
+      "lastUsed": {
+        "$date": {
+          "$numberLong": "1750341480933"
+        }
+      },
+      "favorite": {
+        "name": "Tugas2",
+        "color": "color3"
+      },
+      "savedConnectionType": "recent",
+      "connectionOptions": {
+        "connectionString": "mongodb://localhost:27017/",
+        "oidc": {
+          "serializedState": "eyJvaWRjUGx1Z2luU3RhdGVWZXJzaW9uIjowLCJzdGF0ZSI6W119"
+        }
+      }
+    }
+  ]
+}
